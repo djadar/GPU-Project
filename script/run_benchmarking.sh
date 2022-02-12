8fda4c0a4d86f1a2c0ac9a6ce00e7a68f4bec17e
djadar <mukamaugusta5@gmail.com>
2022-02-12 17:43:11 +0100

    changing scripts

--- script/run_benchmarking.sh
+++ script/run_benchmarking.sh
@@ -5,12 +5,22 @@
 
 #touch $OUTPUT_FILE
 
-OUTPUT_FILE=`data.txt`
+#OUTPUT_FILE=`data.txt`
 
 for i in 128 256 512 1024 2048; do
-    for ker in 3 5 7; do
-        echo "Size: $i" >> data.txt;
-        echo "kernel: $ker" >> data.txt;
-        ../build/edge_cpu --WC=$i --HC=$i --WK=$ker >> data.txt;
+    for ker in 3 5 ; do
+        echo "Size: $i" >> data_gpu.txt;
+        echo "kernel: $ker" >> data_cpu.txt;
+        ../build/edge_cpu --WC=$i --HC=$i --WK=$ker >> data_cpu.txt;
+    done ;
+done
+
+for i in 128 256 512 1024 2048; do
+    for ker in 3 5; do
+        for cho in 1 2; do
+            echo "Size: $i" >> data_gpu.txt;
+            echo "kernel: $ker" >> data_gpu.txt;
+            ../build/edge_cuda --WC=$i --HC=$i --WK=$ker --choice=$cho>> data_gpu.txt;
+        done;
     done ;
 done
